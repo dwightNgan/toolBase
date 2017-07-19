5c2ec8138a96f4bd2bbb4f801a53407a7437b6b2
dwightNgan <yjs@xbed.com.cn>
2017-07-19 23:45:57 +0800

    regEx.js:add:add 2 regEx:idCard(chinese) and chineseCharater

--- regEx.js
+++ regEx.js
@@ -1,4 +1,6 @@
 let cellphone=/^1[3578]\d{9}$/,
     email=/^[a-z_0-9.-]{1,64}@([a-z0-9-]{1,200}.){1,5}[a-z]{1,6}$/,
-    bankcard=/^(\d{16}|\d{19})$/;
-console.log(bankcard.test('123456789012345678900'));
+    bankcard=/^(\d{16}|\d{19})$/,
+    idCard=/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/,
+    chineseCharacter=/[\u4e00-\u9fa5]+/g;
+console.log(('111我们sss').search(chineseCharacter));
